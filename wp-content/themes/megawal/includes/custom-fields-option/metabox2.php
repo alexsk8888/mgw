f411896b2f9ce8bef6ec1343762bf1d80d69741b
Somak Alexey <alexsk8888@gmail.com>
2021-12-03 16:14:39 +0200

    tab_skilet

---
+++ wp-content/themes/megawal/includes/custom-fields-option/metabox2.php
@@ -0,0 +1,52 @@
+<?php
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+
+Container::make("post_meta", "Первичная уборка")
+
+    ->set_priority("high")
+    ->show_on_template("templates/front-page.php")
+ 
+    ->add_fields( array(
+        Field::make( 'image', 'first_clean_background', __( ' Фоновое изображение' ) )->set_width( 50 ),
+        Field::make( 'rich_text', 'first_clean_text', __( ' Текст' ))->set_width( 50 )
+       
+    ) );
+
+Container::make("post_meta", "Первичная уборка")
+
+    ->set_priority("high")
+    ->show_on_template("templates/front-page.php")
+ 
+    ->add_fields( array(     
+        Field::make( 'complex', 'why_are_we', 'Блок "плитка"  максимум 20шт' )->set_layout( 'tabbed-horizontal' )->set_max( 20 )
+        ->add_fields( array( 
+              Field::make( 'rich_text', 'why_are_we_text', __( ' Текст' ))->set_width( 50 ),
+              Field::make( "checkbox", "why_color", "Белый цвет текста" )->set_width( 10 ),
+              Field::make( 'image', 'why_are_we_background', __( ' Фоновое изображение' ) )->set_width( 10 ),
+             
+				 
+
+        ) )       
+    ) );
+
+
+    Container::make("post_meta", "Виды профилей")
+    // ->set_context('side')
+    ->set_priority("high")
+    ->show_on_template("templates/front-page.php")
+    // video box
+    ->add_fields( array(
+
+        Field::make( 'complex', 'views', 'кнопка  максимум 10шт' )->set_layout( 'tabbed-horizontal' )->set_max( 10 )
+            ->add_fields( array(
+                Field::make( 'textarea', 'view_title', __( ' Заголовок кнопки' ))->set_width( 15 ),
+                Field::make( 'image', 'view_icon', __( ' Изображение' ) )->set_width( 10 ),
+                Field::make( 'textarea', 'view_alt', __( ' "Alt" Фонового изображения' ))->set_width( 15 ),
+                Field::make( 'rich_text', 'view_description', __( ' Описание' ))->set_width( 60 ),
+            ) ),
+
+
+    ) );
